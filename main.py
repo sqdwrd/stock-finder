ed3386bd1e8f4f336d898eaed47e05110b13d88f
sqidward1208 <sqidward1208@naver.com>
2020-07-08 21:37:31 +0900

    testing

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 import sys
+import subprocess
 
 hi_url = 'http://www.e-himart.co.kr/app/goods/goodsDetail?goodsNo=0003948195'
 nvr_url = 'https://search.shopping.naver.com/detail/detail.nhn?nvMid=20985197108&query=%25EB%25A7%2581%25ED%2594%25BC%25ED%258A%25B8&NaPm=ct%3Dkbofpcqw%7Cci%3D15eb519cd6f277cf5994fb88bb92649e3bc9d4cb%7Ctr%3Dslsl%7Csn%3D95694%7Chk%3D3c9d80bd94fd8219167c801ab7cb962c46741e44'
@@ -50,8 +51,8 @@ else:
 
 f = open(txt, 'a')
 if isdebug == True:
-    print(timenow() + ' ----------------디버깅 시작---------------')
-    f.write(timenow() + ' ----------------디버깅 시작---------------\n')
+    print(timenow() + ' -------------디버깅 시작------------')
+    f.write(timenow() + ' -------------디버깅 시작------------\n')
 else:
     print(timenow() + ' ----------------시작----------------')
     f.write(timenow() + ' ----------------시작----------------\n')
@@ -118,6 +119,31 @@ while True:
         else:
             print(nvr_LocalTime + ' ' + hi_result + '; ' + nvr_result)
             f.write(nvr_LocalTime + ' ' + hi_result + '; ' + nvr_result + '\n')
+
+        if hi_result == '보유':
+            try:
+                htmlf = open(nvr_localtime + 'html/하이마트.html', 'w')
+            except FileNotFoundError:
+                subprocess.run(['mkdir', 'html'])
+                htmlf = open(nvr_localtime + 'html/하이마트.html', 'w')
+            htmlf.write(hi_html)
+            htmlf.close()
+
+        nvr_intprice = "0"
+        for i in nvr_price:
+            try:
+                int(i)
+                nvr_intprice = nvr_intprice + i
+        nvr_intprice = int(nvr_intprice)
+        if nvr_price <= 90000:
+            try:
+                htmlf = open(nvr_localtime + 'html/네이버.html', 'w')
+            except FileNotFoundError:
+                subprocess.run(['mkdir', 'html'])
+                htmlf = open(nvr_localtime + 'html/네이버.html', 'w')
+            htmlf.write(nvr_html)
+            htmlf.close()
+
         f.close()
         driver.close()
         time.sleep(60)
